9ae723893770223dd05240f7a5b51165ea919faa
api2mcp <dev@api2mcp>
2026-05-22 14:40:31 +0800

    auto: /root/private_data/sun/code/api2mcp/main.py

--- main.py
+++ main.py
@@ -111,6 +111,7 @@ AUTH_PAGE_HTML = """\
       <input type="password" name="secretKey" required placeholder="Your SK">
     </label>{error_html}
     <button type="submit">Authenticate</button>
+    <p class="user">User: <strong>ac1npa3sf2</strong></p>
   </form>
 </div>
 </body>
